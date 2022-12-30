b29d0800ea1ce1e989c78ff956ec6a5e11e3d7c3
JieunOh <0909oje@naver.com>
2022-12-30 18:57:49 +0900

    :sparkles: JPA 활용

--- hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
@@ -2,11 +2,21 @@ package hello.hellospring.service;
 
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private EntityManager em;
+    public SpringConfig(EntityManager em) {
+        this.em = em;
+    }
+
 
     @Bean
     public MemberService memberService(){
