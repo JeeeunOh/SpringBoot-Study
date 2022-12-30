4718b373591a080d4473cf2368e6ec54dab03f90
JieunOh <0909oje@naver.com>
2022-12-30 19:00:01 +0900

    :sparkles: Data JPA

--- hello-spring/src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -3,7 +3,6 @@ package hello.hellospring.repository;
 import hello.hellospring.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.LongSummaryStatistics;
 import java.util.Optional;
 
 public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
--- hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,30 +1,19 @@
 package hello.hellospring.service;
 
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    private final MemberRepository memberRepository;
+
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
-
-
     @Bean
-    public MemberService memberService(){
-        return new MemberService(memberRepository());
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
-    }
 }
